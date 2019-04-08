3c578cabbb82510aca97225e8c995d7a21c3bf3a
Cami Blanch <caminblanch@gmail.com>
2019-04-08 16:51:54 -0600

    Stubbed out suggestion components

---
+++ src/components/Suggestion.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>Suggestion!</div>
+</template>
+
+<script>
+	export default {
+		name: "Suggestion",
+		props: {
+			"suggestions": Array,
+			"selectSuggestion": Function,
+		},
+	};
+</script>
+
+<style lang="scss" scoped>
+	@import "Suggestion";
+</style>
---
+++ src/components/Suggestions.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		Hi
+		<div v-for="suggestion in suggestions">
+			<Suggestion
+				:suggestion="suggestion"
+				selectSuggestion="() => selectSuggestion(suggestion)"
+			/>
+		</div>
+	</div>
+</template>
+
+<script>
+	import Suggestion from "./Suggestion";
+
+	export default {
+		name: "Suggestions",
+		components: {Suggestion},
+		props: {
+			"suggestions": Array,
+			"selectSuggestion": Function,
+		},
+	};
+</script>
