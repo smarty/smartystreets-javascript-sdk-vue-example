720dfd42ef8b87ccf4e97091b9d154a3388492d0
Cami Blanch <caminblanch@gmail.com>
2019-04-09 16:58:10 -0600

    Removed all unnecessary calls to "this" and fixed functions

--- src/components/Suggestion.vue
+++ src/components/Suggestion.vue
@@ -1,13 +1,34 @@
 <template>
-	<div>Suggestion!</div>
+	<div
+		v-bind:class="buildResultHoverClass()"
+		v-on:click="() => selectSuggestion(suggestion)"
+		v-on:mouseenter="() => setIsHovered(true)"
+		v-on:mouseleave="() => setIsHovered(false)"
+	>
+		{{ suggestion.text }}
+	</div>
 </template>
 
 <script>
 	export default {
 		name: "Suggestion",
 		props: {
-			"suggestions": Array,
+			"suggestion": Object,
 			"selectSuggestion": Function,
+		},
+		data() {
+			return {
+				isHovered: false,
+			};
+		},
+		methods: {
+			setIsHovered(isHovered) {
+				this.isHovered = isHovered;
+			},
+			buildResultHoverClass() {
+				const className = "autocomplete--suggestion";
+				return this.isHovered ? className + " autocomplete--suggestion-hover" : className;
+			},
 		},
 	};
 </script>
