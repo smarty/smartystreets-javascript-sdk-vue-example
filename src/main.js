e1ddbbd124ee465c54c2597a8fa2d4df5f8f9706
Cami Blanch <caminblanch@gmail.com>
2021-09-02 15:13:33 -0600

    Updates to Vue 3 so we can use the updated version of Parcel

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
-import Vue from "vue";
-import App from "./App.vue";
+import { createApp } from "vue";
+import App from "./App";
 
-new Vue({
-	el: "#app",
-	render: h => h(App),
-});
+const app = createApp(App);
+app.mount("#app");
